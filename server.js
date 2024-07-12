6d61d6aa05e9ceb76a6f40a92dcebf49eb716b98
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-07-12 13:12:59 -0600

    changed some stuff

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/api-key', (req, res) => {
+  res.json({ apiKey: process.env.OPENAI_API_KEY });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
